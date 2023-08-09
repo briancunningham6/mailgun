43a8e7de452be5a56f0d88d00ac64161402fb70f
Brian Cunningham <brianc@kseudo.com>
2023-08-09 10:07:14 +0100

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Mailgun.Mixfile do
   def project do
     [app: :mailgun,
      version: "0.1.3",
-     elixir: "~> 1.0",
+     elixir: "~> 1.1",
      deps: deps,
      package: [
        contributors: ["Chris McCord"],
